c019d3bcddae08a9720320582445320cd8b47dc2
kemochka <kemaevak@yandex.ru>
2025-06-11 11:44:07 +0300

    fix connecting with db

--- src/main/java/com/example/kafka_consumer_demo/model/Order.java
+++ src/main/java/com/example/kafka_consumer_demo/model/Order.java
@@ -4,12 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
-@Entity
 public record Order(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        long orderId,
+        String orderId,
         String productName
 ) {
 
--- src/main/java/com/example/kafka_consumer_demo/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.kafka_consumer_demo.repository;
-
-import com.example.kafka_consumer_demo.model.Order;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends CrudRepository<Order, Long> {
-}
